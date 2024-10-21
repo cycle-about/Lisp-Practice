6c3e712030fbe1e3c3f879d815012111a5138dc6
Laura Vail <vail.laura@gmail.com>
2024-10-21 19:50:39 -0400

    Chapter 2 and most of chapter 3

--- 2_little_schemer.rkt
+++ 2_little_schemer.rkt
@@ -11,6 +11,7 @@
 ;	)
 ;)
 
+#|
 ; from chatgpt, book does not define it
 ; define the atom? function
 (define (atom? x)
@@ -22,14 +23,25 @@
 		(cond
 			((null? l) #t)
 			((atom? (car l)) (lat? (cdr l)))
-			(else #f)
-		)
-	)
-)
+			(else #f))))
 
 (displayln (lat? '(1 2 3 4)))
 (define l '("Jack" "Sprat" "could" "eat" "no" "chicken" "fat"))
 (displayln (lat? l))
 (define l-2 '(("Jack" "Sprat") "could" "eat" "no" "chicken" "fat"))
 (displayln (lat? l-2))
+|#
 
+; book p22
+(define member?
+	(lambda (a lat)
+		(cond
+			((null? lat) #f)
+			(else (or (eq? (car lat) a) (member? a (cdr lat)))))))
+		; if lat is empty:return true
+		; else lat NOT empty: check whether car equals a OR if a is in cdr of lat
+; because of lazy evaluation: if car lat is true, OR statement is true, so return
+;     if car lat is false, OR statement depends on evaluating cdr lat, so continues (recursive case)
+
+(displayln (member? "meat" '("mashed" "potatoes" "and" "meat" "gravy")))
+(displayln (member? "meat" '("mashed" "potatoes" "and" "gravy")))
---
+++ 3_little_schemer.rkt
@@ -0,0 +1,149 @@
+#lang racket
+
+; Chapter 3. Cons the Magnificent
+
+#|
+
+; version that removes matching word and everything before it
+(define rember-1
+	(lambda (a lat)
+		(cond
+			((null? lat) (displayln '() )) 		; if lat is empty, print an empty list
+			(else (cond 											; else lat not empty
+				((eq? (car lat) a) (cdr lat))		; if car lat equals a: return cdr lat
+				(else (rember-1 a (cdr lat))) 	; else car lat not equals a: call rember-1 on cdr lat
+			))
+		)
+	)
+)
+
+; corrected version
+(define rember-2
+	(lambda (a lat)
+		(cond
+			((null? lat) (displayln '() ))   										; if lat is empty, print an empty list
+			(else (cond
+				((eq? (car lat) a) (cdr lat)) 										; if car of lat equals a: return cdr lat
+				(else (cons (car lat) (rember-2 a (cdr lat))))  	; if car lat not equals a: add car lat to rember a with cdr lat (pick up car lat at end)
+			))
+		)
+	)
+)
+
+; simplified version that does the same as 2
+(define rember-3
+	(lambda (a lat)
+		(cond
+			((null? lat) (displayln '() )) 										; second part has to be a function, empty list throws error
+			((eq? (car lat) a) (cdr lat)) 										; if car lat equals a: return cdr lat
+			(else (cons (car lat) (rember-3 a (cdr lat)))) 		; else keep cdr lat to add onto result of rember-3 a with cdr lat
+		)
+	)
+)
+
+(displayln (rember-3 "bacon" '() )) 																; #<void>   [after the print statement by function]
+(displayln (rember-3 "bacon" '("bacon" "lettuce" "and" "tomato"))) 	; (lettuce and tomato)
+(displayln (rember-3 "and" '("bacon" "lettuce" "and" "tomato"))) 		; (bacon lettuce tomato)
+
+
+(define firsts
+	(lambda (l)
+		(cond
+			((null? l) '() ) 																; this one has empty list in second part
+			(else (cons (car (car l)) (firsts (cdr l)))) 		; get the car of the car of l, add it to first on cdr l
+		)
+	)
+)
+
+(displayln (firsts '(("a" "b") ("c" "d") ("e" "f")) )) 		; (a c e)
+
+
+; adds 'new' to the right of 'old' in a list
+(define insertR
+	(lambda (new old lat)
+		(cond
+			((null? lat) (displayln '() ))
+			(else (cond
+				((eq? (car lat) old) (cons old (cons new (cdr lat))))     ; if car lat equals old: get old, add to cons of new onto (cdr lat)
+					(else (cons (car lat) (insertR new old (cdr lat)))) 		; else: get car lat, add to insertR called on new old (cdr lat)
+			))
+		)
+	)
+)
+
+(define new "topping")
+(define old "fudge")
+(define lat '("ice" "cream" "with" "fudge" "for" "dessert"))   ; (ice cream with fudge topping for dessert)
+(displayln (insertR new old lat))
+
+; adds 'new' to the left of 'old' in a list
+; difference is order of cons in the matching case
+(define insertL-1
+	(lambda (new old lat)
+		(cond
+			((null? lat) (displayln '() ))
+			(else (cond
+				((eq? (car lat) old) (cons new (cons old (cdr lat)))) 		; if car lat equals old: get new, add it to result of old cons to cdr lat
+					(else (cons (car lat) (insertL-1 new old (cdr lat)))) 		; else: get car lat, add to result of insertL-1 on new old cdr lat
+			))
+		)
+	)
+)
+
+; different way to do insertL
+; "(cons new (cons old (cdr lat))" is the same as "cons new lat", because there it's true that car lat equals old
+(define insertL-2
+	(lambda (new old lat)
+		(cond
+			((null? lat) (displayln '() ))
+			(else (cond
+				((eq? (car lat) old) (cons new lat)) 												; if car lat equals old: get new, add it to current list
+					(else (cons (car lat) (insertL-2 new old (cdr lat)))) 		; else: get car lat, add to result of insertL-1 on new old cdr lat
+			))
+		)
+	)
+)
+
+(define new "topping")
+(define old "fudge")
+(define lat '("ice" "cream" "with" "fudge" "for" "dessert"))   ; (ice cream with topping fudge for dessert)
+(displayln (insertL-2 new old lat))
+
+|#
+
+(define subst-1
+	(lambda (new old lat)
+		(cond
+			((null? lat) (displayln '() ))
+			(else (cond
+				((eq? (car lat) old) (cons new (cdr lat)))
+				(else (cons (car lat) (subst-1 new old (cdr lat))))
+			))
+		)
+	)
+)
+
+(displayln (subst-1 "fudge" "topping" '("ice" "cream" "with" "topping" "for" "dessert") )) 		; (ice cream with fudge for dessert)
+
+
+; substitute 'new' for the first occurrence of either 'o1' or 'o2'
+(define subst-2
+	(lambda (new o1 o2 lat)
+		(cond
+			((null? lat) (displayln '() ))
+			(else (cond
+				((or (eq? (car lat) o1) (eq? (car lat) o1)) (cons new (cdr lat)))   ; if car lat equals o1 or o2, cons new onto cdr lat
+				
+				(else (cons (car lat) (subst-2 new o1 o2 (cdr lat))))
+			))
+		)
+	)
+)
+
+(define new "vanilla")
+(define o1 "chocolate")
+(define o2 "banana")
+(define lat '("banana" "ice" "cream" "with" "chocolate" "topping"))
+(displayln (subst-1 new o2 lat)) 							; (vanilla ice cream with chocolate topping)
+(displayln (subst-2 new o1 o2 lat))  					; (banana ice cream with vanilla topping)      * subst-2 found the 'end' value first
+; it finds the END value first
