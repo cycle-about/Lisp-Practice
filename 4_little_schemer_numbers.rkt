8c2532a92ff150a6b84dc044e57ebd52c970c427
Laura Vail <vail.laura@gmail.com>
2024-11-03 17:15:07 -0500

    Continued chapter 4, question about ch 4 function

--- 4_little_schemer_numbers.rkt
+++ 4_little_schemer_numbers.rkt
@@ -109,8 +109,147 @@
 	)
 )
 
-(displayln (equals 5 5))
-(displayln (equals 3 7))
-(displayln (equals 0 0))  ; #t
-(displayln (equals -1 -1))  ; command hangs
+; (displayln (equals 5 5))
+; (displayln (equals 3 7))
+; (displayln (equals 0 0))  ; #t
+; (displayln (equals -1 -1))  ; command hangs
+
+(define exponent
+	(lambda (n m)
+		(cond
+			((zero? m) 1)
+			(else (multiply n (exponent n (sub1 m))))
+		)
+	)
+)
+
+; (displayln (exponent 2 4))
+
+(define division
+	(lambda (n m)
+		(cond
+			((lesser n m) 0)
+			(else (add1 (division (subtraction n m) m)))
+		)
+	)
+)
+
+; (displayln (division 15 4))
+
+(define length
+	(lambda (lat)
+		(cond
+			((null? lat) 0)
+			(else (add1 (length (cdr lat))))
+		)
+	)
+)
+
+; (displayln (length '("hello") ))  ; 1
+; (displayln (length '(4 5 7) )) ; 3
+
+; return the item at index n
+; scheme is 1-indexed, index 0 is an error
+(define pick
+	(lambda (n lat)
+		(cond
+			((zero? (sub1 n)) (car lat))
+			(else (pick (sub1 n) (cdr lat)))
+		)
+	)
+)
+
+; (displayln (pick 2 '(0 1 2 3 4) ))  ; 1
+; (displayln (pick 0 '(0 1 2 3 4) ))  ; cdr: contract violation, expected: pair?, given: '()
+
+; return the list with item at index n removed
+(define rempick
+	(lambda (n lat)
+		(cond
+			((zero? (sub1 n)) (cdr lat))
+			(else (cons (car lat) (rempick (sub1 n) (cdr lat))))
+		)
+	)
+)
+
+; (displayln (rempick 2 '(0 1 2 3 4) ))  ; (0 2 3 4)
+; (displayln (rempick 1 '(0 1 2 3 4) ))  ; (1 2 3 4)
+
+; removes all numbers from the list
+(define no-nums
+	(lambda (lat)
+		(cond
+			((null? lat) (displayln '("list is null") ))
+			; ((null? lat) ())  														; "missing procedure expression", can't pass an actual empty expression for the 'do nothing' case
+			(else (cond
+				((number? (car lat)) (no-nums (cdr lat)))
+				(else (cons (car lat) (no-nums (cdr lat))))
+			))
+		)
+	)
+)
+
+; (displayln (no-nums '("hello" 3 "world" 7 "!" 4 "6") ))  			; (list is null) (hello world ! 6 . #<void>)
+; (displayln (no-nums '("hello" "world") )) 										; (list is null) (hello world . #<void>)
+; example inputs from book
+; (displayln (no-nums '(5 "pears" 6 "prunes" 9 "dates") )) 				; still hits empty list
+; (displayln (no-nums '(5 6 9 ) )) 																; only numbers: still hits empty list
+; (displayln (no-nums '(5 6 9 "hi" "bye") )) 											; has string but ends with number: still hits empty list
+; ???? this seems to always hit the empty list, unlike other example functions
+
+
+(define all-nums
+	(lambda (lat)
+		(cond
+			((null? lat) (displayln '() ))
+			(else
+				(cond
+					((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+					(else (all-nums (cdr lat)))
+				)
+			)
+		)
+	)
+)
+
+; (displayln (all-nums '("hello" 3 "world" 7 "!" 4 "6") ))  			; () (3 7 4 . #<void>)
+; (displayln (all-nums '("hello" "world") )) 											; () #<void>
+; ???? this one also always hits the empty list clause
+
+
+; returns true or false depending on whether two atoms are the sam
+(define eqan?
+	(lambda (a1 a2)
+		(cond
+			((and (number? a1) (number? a2)) (= a1 a2))  			; if a1 and a2 are numbers: return result of numeric equality check
+			((or (number? a1) (number? a2)) #f) 							; if only one of them is a number: return false
+			(else (eq? a1 a2)) 																; if neither of those: return result of non-numeric equality check
+		)
+	)
+)
+
+; (displayln "running eqan?")
+; (displayln (eqan? 3 4))    	; #f
+; (displayln (eqan? "hi" 3))  ; #f
+; (displayln (eqan? 4 4)) 		; #t
+; (displayln (eqan? #f #f)) 	; #t
+
+; counts the number of times atom appears in lat
+(define occur
+	(lambda (a lat)
+		(cond
+			((null? lat) 0)
+			(else
+				(cond
+					((eq? (car lat) a) (add1 (occur a (cdr lat))))
+					(else (occur a (cdr lat)))
+				)
+			)
+		)
+	)
+)
+
+(displayln (occur "hi" '("hi" "bye" 3 "hi" 1) )) 	; 2
+(displayln (occur 3 '("hi" "bye" 3 "hi" 1) )) 		; 1
+(displayln (occur "" '("" "hi" 3) )) 							; 1
 
---
+++ questions.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+;;; Question: why does Example 1 not print out "void" and "list is empty" with non-empty inputs, while the similar Example 2 does?
+
+;; Example 1. Returns list with an item removed
+(define rember-3
+	(lambda (a lat)
+		(cond
+			((null? lat) (displayln '(list is empty) )) 			; if lat is null: display list is empty
+			((eq? (car lat) a) (cdr lat)) 										; if car lat equals a: return cdr lat
+			(else (cons (car lat) (rember-3 a (cdr lat)))) 		; if neither of those: store car lat, add it to result of rember-3 a with cdr lat
+		)
+	)
+)
+
+(displayln '("############################# "))
+(displayln '("#### Results of rember-3 ####"))
+(displayln (rember-3 "bacon" '("bacon" "lettuce" "and" "tomato"))) 	; (lettuce and tomato)
+(displayln (rember-3 "and" '("bacon" "lettuce" "and" "tomato"))) 		; (bacon lettuce tomato)
+
+; how to intentionally get the void message from this function
+(displayln (rember-3 "bacon" '() )) 																; (list is empty) #<void>
+
+
+
+;; Example 2. Returns a list with numbers removed
+
+(define no-nums
+	(lambda (lat)
+		(cond
+			((null? lat) (displayln '("list is empty") )) 				; if lat is null: display list is empty
+			(else 																								; if not null, continue
+				(cond 
+					((number? (car lat)) (no-nums (cdr lat)))  				; if car lat is number: run no-nums on cdr lat
+					(else (cons (car lat) (no-nums (cdr lat))))  			; if not: add car lat to result of no-nums run on cdr lat
+				)
+			)
+		)
+	)
+)
+
+(displayln '("############################ "))
+(displayln '("#### Results of no-nums #### "))
+(displayln (no-nums '("bacon" "lettuce" "and" "tomato"))) 		; only strings 					--> (list is empty) (bacon lettuce and tomato . #<void>)
+(displayln (no-nums '("bacon" "lettuce" "and" "tomato" 3))) 	; both 								 	--> (list is empty) (bacon lettuce and tomato . #<void>)
+(displayln (no-nums '(3 "bacon" "lettuce" "and" "tomato" ))) 	; both 								 	--> (list is empty) (bacon lettuce and tomato . #<void>)
+(displayln (no-nums '(5 6 9))) 															; only numbers 					--> (list is empty) #<void>
+(displayln (no-nums '() ))  																	; empty list 						--> (list is empty) #<void>
