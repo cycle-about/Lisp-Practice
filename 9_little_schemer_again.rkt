964f409c7bb29340bbc28a62687c9244e9203d6e
Laura Vail <vail.laura@gmail.com>
2025-11-28 20:40:50 -0500

    Ch 9 through function 11

--- 9_little_schemer_again.rkt
+++ 9_little_schemer_again.rkt
@@ -58,6 +58,52 @@
 	)
 )
 
+;; from chapter 7
+;; reverses the two components of a pair [unlike a set, a pair has defined order]
+(define revpair
+	(lambda (pair)
+		(build (second pair) (first pair))
+	)
+)
+
+;; from chapter 4
+(define one?
+	(lambda (n)
+		(= n 1)
+	)
+)
+
+;; from chapter 4
+(define division
+	(lambda (n m)
+		(cond
+			((lesser n m) 0)
+			(else (add1 (division (subtraction n m) m)))
+		)
+	)
+)
+
+;; from chapter 4
+(define lesser
+	(lambda (n m)
+		(cond
+			((zero? m) #f)
+			((zero? n) #t)
+			(else (lesser (sub1 n) (sub1 m)))
+		)
+	)
+)
+
+;; from chapter 4
+(define subtraction
+	(lambda (n m)
+		(cond
+			((zero? m) n)
+			(else (sub1 (subtraction n (sub1 m))))
+		)
+	)
+)
+
 ;;;;;;;;;;;; END HELPER FUNCTIONS ;;;;;;;;;;;;
 
 ;; FUNCTION 1
@@ -191,6 +237,39 @@
 	)
 )
 
+#|
+'(((a b) c) d) ))
+
+					 root
+					 /   \
+				node 		d  			d: 0 left turns, 2^0 = 1
+				/		\
+		node		 c 					c: 1 left turn, 2^1 = 2
+    /  \ 								b: 2 left turns, 2^2 = 4
+   a    b 							a: 3 left turns, 2^3 = 8
+ 												TOTAL: 15
+
+
+'((a (b c)) (d e))
+level 1 items: node (a (b c)) | node (d e)
+level 2 items: a, node (b c) | d, e
+level 3 items: b | c
+
+				root
+				/   \
+	    node  node
+ 	   /  \     / \ 
+  	a  node  d   e
+			 /  \
+		 	b    c
+
+a: 2 left, 2^2 = 4
+b: 2 left, 2^2 = 4
+c: 1 left, 2^1 = 2
+d: 1 left, 2^1 = 2
+e: 0 left, 2^0 = 1
+TOTAL: 13
+
 (displayln (weight* 'a))                        ; 1
 (displayln (weight* '(a b) ))                   ; 3
 (displayln (weight* '(a (b c)) ))               ; 5
@@ -198,3 +277,79 @@
 (displayln (weight* '((1 2) (3 4)) ))           ; 9
 (displayln (weight* '((a (b c)) (d e)) ))       ; 13
 (displayln (weight* '(((a b) c) d) ))           ; 15
+|#
+
+;; FUNCTION 8
+;; swap the components of pairs when first component is a pair
+;; unlike align, it uses revpair
+;; shuffle is not total because it swaps components of pair again
+;; total function, in programming: terminates and retuns a value for every possible input
+;; partial function: for some inputs it won't return a result (infinte loop, error, or is undefined)
+(define shuffle
+	(lambda (pora)
+		(cond
+			((atom? pora) pora)
+			((a-pair? (first pora)) (shuffle (revpair pora)))
+			(else (build (first pora) (shuffle (second pora))))
+		)
+	)
+)
+
+; (displayln (shuffle '(a (b c)) )) 		; (a (b c))
+; (displayln (shuffle '(a b) ))  				; (a b)
+; (displayln (shuffle '((a b) (c d)) )) ; infinite loop
+
+;; FUNCTION 9
+;; unknown if total
+(define C
+	(lambda (n)
+		(cond
+			((one? n) 1)
+			(else 
+				(cond
+					((even? n) (C (division n 2)))
+					(else (C (add1 (* 3 n))))
+				)
+			)
+		)
+	)
+)
+
+; (displayln (C 0)) ; hangs
+; (displayln (C 6)) ; 1
+
+;; FUNCTION 10
+;; Ackermann function: total, computable, grows extremely fast, not primitve recursive
+;; Not tail-recursive: the third clause nests a call inside another, causing very deep recursion
+;; primitive recursive: those computable by for loops where the number of iterations is bounded in advance by the input 
+(define A
+	(lambda (n m)
+		(cond
+			((zero? n) (add1 m))
+			((zero? m) (A (sub1 n) 1))
+			(else (A (sub1 n) (A n (sub1 m))))
+		)
+	)
+)
+
+; (displayln (A 1 0)) ; 2
+; (displayln (A 1 1)) ; 3
+; (displayln (A 2 2)) ; 7
+; (displayln (A 4 3)) ; hangs
+
+;; FUNCTION 11
+;; surprised this prints even without displayln
+;(displayln
+	(((lambda (mk-length) (mk-length mk-length))
+		(lambda (mk-length)
+			(lambda (l)
+				(cond
+					((null? l) 0)
+					(else (add1 ((mk-length mk-length) (cdr l))))
+				)
+			)
+		)
+	)
+	'(apples)
+	)
+;)
